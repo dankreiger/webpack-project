5db7fdd002fe269940daa0554cbe778468b96120
Dan Kreiger <dan@dankreiger.com>
2018-02-22 22:32:18 +0100

    Make logging waggily again

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ require("./main.css");
 require("./index.html");
 
 const puppy = () => {
-  console.log("what up puppy");
+  console.log("%c what up puppy ", 'background: #222; color: #bada55');
   const audio = new Audio('http://soundbible.com/mp3/puppy-barking_ds.mp3');
   audio.play();
 }
