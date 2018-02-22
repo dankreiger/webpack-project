71dd8a9bab1310af16c0f7c2703c4e721f6c715d
Dan Kreiger <dan@dankreiger.com>
2018-02-22 23:21:44 +0100

    Add babel runtime

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
+require("babel-runtime/regenerator");
 require("./main.css");
 require("./index.html");
 
-const puppy = () => {
+const puppy = (args) => {
+  const {a, b} = args
   console.log("%c what up puppy ", 'background: #222; color: #bada55');
   const audio = new Audio('http://soundbible.com/mp3/puppy-barking_ds.mp3');
   audio.play();
 }
 
+const dog = async() => {
+  await console.log("Der Welpe wird groß",  a, b);
+  console.log("Big")
+}
+
 const text = document.querySelector('h1');
 text.addEventListener("click", puppy);
+
+
+dog({a: 1, b: 2})
