bb55122e1206246aa82bb921341447134b58e637
Dan Kreiger <dan@dankreiger.com>
2018-02-23 00:22:52 +0100

    Add express

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 require("babel-runtime/regenerator");
+require("webpack-hot-middleware/client");
 require("./main.css");
 require("./index.html");
 
-const puppy = (args) => {
-  const {a, b} = args
+const puppy = () => {
   console.log("%c what up puppy ", 'background: #222; color: #bada55');
   const audio = new Audio('http://soundbible.com/mp3/puppy-barking_ds.mp3');
   audio.play();
 }
 
-const dog = async() => {
+const dog = async(args) => {
+  const {a, b} = args
+
   await console.log("Der Welpe wird groß",  a, b);
   console.log("Big")
 }
